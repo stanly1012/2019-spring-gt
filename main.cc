c31bc440feb7db2589e887cbfcf27be92eb210a1
stanly1012 <stanly1012@gmail.com>
2019-06-04 21:06:14 +0800

    can calculate degree of each vertex

--- main.cc
+++ main.cc
@@ -17,44 +17,37 @@ NetworkManager *nm = new NetworkManager();
 int main(int argc, char** argv){
     int vertexcount;
     int j,k;
-    int degree;
+    int degree[10];
     Vertex *node;
     vector<Vertex*> name;
-    //int name[10];
-    /* start your program */
     nm->interpret("topo.txt");
-    
-    
-    
-    /*if(nm->connected("a","b")==0){
-        i+=1;}
-    if(nm->connected("a","c")==0){
-        i+=1;}
-    if(nm->connected("a","d")==0){
-        i+=1;}
-        
-    if(i%2==1){
-        printf("odd");}
-        
-    nm->print_all_e();*/
-    
-   
+ /////////////////////////////////////////////
     node = nm->get_all_nodes();
     while(node!=0){
         name.push_back(node);
         node=node->next;
     }
     vertexcount=name.size();
+////////////////////////////////////////////////    
     
     for(j=0; j<vertexcount; j++){
+        degree[j]=0;
         for(k=0; k<vertexcount; k++){
             if(nm->connected(name[j]->name,name[k]->name)==0)
-                degree+=1;
+                degree[j]+=1;
+            else
+                degree[j]==degree[j];
         }
     }
+    
+//////////////////////////////////////////////
 
- 
-    cout<<degree;
+    cout<<degree[0]<<endl;
+    cout<<degree[1]<<endl;
+    cout<<degree[2]<<endl;
+    cout<<degree[3]<<endl;
+    cout<<degree[4]<<endl;
+
 
     return 0;
 }

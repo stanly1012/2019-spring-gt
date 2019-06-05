5987f9240c9373d973b6761e5eb50c544f6ced21
stanly1012 <stanly1012@gmail.com>
2019-06-05 23:12:41 +0800

    debug

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include "gplot.h"
 #include "path.h"
 #include <vector>
+#include <string>
 using namespace std;
 
 // create NetworkManager first
@@ -17,9 +18,9 @@ NetworkManager *nm = new NetworkManager();
 int main(int argc, char** argv){
     int vertexcount;
     int j,k;
-    int m=0;
+    int a=0;
     int degree[10];
-    string oddvertex[10];
+    vector<Vertex*> oddvertex;
     Vertex *node;
     vector<Vertex*> name;
     nm->interpret("topo.txt");
@@ -31,33 +32,59 @@ int main(int argc, char** argv){
     }
     vertexcount=name.size();
 ////////////////////////////////////////////////find odd vertex     
+    
+
     for(j=0; j<vertexcount; j++){
         degree[j]=0;
+        
         for(k=0; k<vertexcount; k++){
             if(nm->connected(name[j]->name,name[k]->name)==0)
                 degree[j]+=1;
-        }
-        
-        if(degree[j]%2==1){
-        oddvertex[m]=name[j]->name;
-        m=+1;
+        }        
+           
+        if(degree[j]%2==1){            
+        oddvertex.push_back(name[j]);
+  
         }
     }
 ////////////////////////////////////////////////////add the edge between two odd vertex
-    nm->connect(oddvertex[0],oddvertex[1]);
+   
+   while(a<oddvertex.size()){
+    nm->connect(oddvertex[a]->name,oddvertex[a+1]->name);
+    a+=2;
+    }
 
-////////////////////////////////////////
+    
+////////////////////////////////////////euler path
+
+
+
+
+
+/////////////////////////////////////////////// 
 
     cout<<degree[0]<<endl;
     cout<<degree[1]<<endl;
     cout<<degree[2]<<endl;
     cout<<degree[3]<<endl;
     cout<<degree[4]<<endl;
-    cout<<oddvertex[0]<<endl;
-    cout<<oddvertex[1]<<endl;
-    cout<<oddvertex[2]<<endl;
+    /*cout<<oddvertex[0]->name<<endl;
+    cout<<oddvertex[1]->name<<endl;
+    cout<<oddvertex[2]->name<<endl;
+    cout<<oddvertex[3]->name<<endl;
+    cout<<oddvertex[4]->name<<endl;*/
+
+    
+    cout<<a<<endl;
+ 
 
     nm->print_all_e();
+    
+    
+    Gplot *gp = new Gplot();
+    gp->gp_add(nm->elist);
+    gp->gp_dump(false);
+    gp->gp_export("plot");
 
     return 0;
 }
